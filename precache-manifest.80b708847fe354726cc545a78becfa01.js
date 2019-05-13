3798310d68ca42e233a297008ea02c50dea7d328
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-13 17:41:51 +0000

    Deploy gabriel-arauj/aula_ci to github.com/gabriel-arauj/aula_ci.git:gh-pages

---
+++ precache-manifest.80b708847fe354726cc545a78becfa01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d989f5c9ed6cbce7334aac56c42b80",
+    "url": "/aula_ci/index.html"
+  },
+  {
+    "revision": "39465e7970fe9e752eb5",
+    "url": "/aula_ci/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/aula_ci/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "39465e7970fe9e752eb5",
+    "url": "/aula_ci/static/js/main.15c0a7ec.chunk.js"
+  },
+  {
+    "revision": "af771879154d4eeab4bb",
+    "url": "/aula_ci/static/js/runtime~main.611b448d.js"
+  }
+]);
